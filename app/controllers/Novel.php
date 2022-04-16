0b56ff6e4021da41a8f38537f6870f9736c9ccee
ThiccPan <ihilmiderian@gmail.com>
2022-04-16 18:51:32 +0700

    database wrapper and select detail

--- app/controllers/Novel.php
+++ app/controllers/Novel.php
@@ -10,5 +10,14 @@
       $this->view('novel/index',$data);
       $this->view('templates/footer');
     }
+
+    public function detail($id){
+      $data['judul'] = "Detail Novel";
+      $data['novel'] = $this->model('Novel_model')->getNovelByID($id);
+      
+      $this->view('templates/header',$data);
+      $this->view('novel/detail',$data);
+      $this->view('templates/footer');
+    }
   }
 ?>

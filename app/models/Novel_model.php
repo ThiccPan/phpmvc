890e78959a0e79fde888d4fa238f22a325ba6679
ThiccPan <ihilmiderian@gmail.com>
2022-04-17 00:41:01 +0700

    add addNovel function on novel model

--- app/models/Novel_model.php
+++ app/models/Novel_model.php
@@ -1,6 +1,7 @@
 <?php 
   class Novel_model{
     private $table = "novel";
+    private $tableStructure = "id, title, status, cur_chapter, link";
     private $db;
 
       public function __construct() {
@@ -18,6 +19,21 @@
         $this->db->query("SELECT * FROM " . $this->table . " WHERE id=:id ");
         $this->db->bind('id',$id);
         return $this->db->single();
+      }
+
+      public function addNovel($id, $title, $status, $cur_chapter, $link)
+      {
+        $query = "INSERT INTO novel ({$this->tableStructure}) VALUES ('', :title,:statuss,:cur_chapter,:link)";
+        $this->db->query($query);
+        $this->db->bind('title',$title);
+        $this->db->bind('statuss',$status);
+        $this->db->bind('cur_chapter',$cur_chapter);
+        $this->db->bind('link',$link);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+
       }
   }
 

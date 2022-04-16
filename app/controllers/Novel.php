a1de348bdea2d374cd83889b80f5719b2f55ba93
ThiccPan <ihilmiderian@gmail.com>
2022-04-17 00:39:57 +0700

    add addNovel function on controller

--- app/controllers/Novel.php
+++ app/controllers/Novel.php
@@ -11,13 +11,26 @@
       $this->view('templates/footer');
     }
 
-    public function detail($id){
+    public function detail($id) {
       $data['judul'] = "Detail Novel";
       $data['novel'] = $this->model('Novel_model')->getNovelByID($id);
       
       $this->view('templates/header',$data);
       $this->view('novel/detail',$data);
       $this->view('templates/footer');
+    }
+
+    public function addNovel() {
+      $id = "";
+      $title = $_POST["title"];
+      $status = $_POST["status"];
+      $cur_chapter = $_POST["cur_chapter"];
+      $link = $_POST["link"];
+
+      if ($this->model('Novel_model')->addNovel($id,$title,$status,$cur_chapter,$link) > 0) {
+        header("Location:" . BASEURL . "views/novel/index.phpp");
+        exit;
+      }
     }
   }
 ?>
